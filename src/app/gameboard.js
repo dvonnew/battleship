5f7a4cd29f572d538486276f81cf8e944d2f9b66
Tyler Phelan <tylerp@vmware.com>
2022-02-15 10:41:57 -0500

    Sometimes use ship type sometimes use shipName

--- src/app/gameboard.js
+++ src/app/gameboard.js
@@ -1,123 +1,121 @@
-import {Ship} from './ships'
-import {shipTypes} from './shipTypes'
+import { Ship } from "./ships";
+import { shipTypes } from "./shipTypes";
 
 class Gameboard {
+  constructor() {
+    this.board = [];
+    this.ships = shipTypes;
+    this.fleet = [];
+    this.build();
+  }
 
-    constructor (){
-        this.board = []
-        this.ships = shipTypes
-        this.fleet = []
-        this.build()
+  build() {
+    for (let i = 0; i < 100; i++) {
+      this.board.push({ hasShip: false, isHit: false });
+    }
+  }
+
+  receiveAttack(location) {
+    this.board[location].isHit = true;
+    if (this.board[location].hasShip !== true) {
+      return;
+    } else {
+      this.attackShip(location);
+    }
+  }
+
+  attackShip(location) {
+    this.fleet.forEach((boat, i) => {
+      if (!boat.position.includes(location)) {
+        return;
+      } else {
+        boat.hit(location);
+        if (!boat.isSunk()) {
+          return;
+        } else {
+          this.fleet.slice(i, 1);
+          console.log(`${boat.name} sank`);
+        }
+      }
+    });
+  }
+
+  placeShip(location, shipName, axis) {
+    let ship = new Ship(shipName, this.ships[shipName]);
+    let locationArray = this.createLocationArray(location, shipName, axis);
+    if (!this.isValidPlacement(locationArray)) {
+      return;
+    } else {
+      locationArray.forEach((loc) => {
+        this.board[loc].hasShip = true;
+        ship.position.push(loc);
+      });
+    }
+    this.fleet.push(ship);
+  }
+
+  createLocationArray(location, shipName, axis) {
+    let locationArray = [];
+    if (axis == "x") {
+      for (let i = 0; i < this.ships[shipName]; i++) {
+        locationArray.push(location + i);
+      }
+    } else {
+      for (let i = 0; i < this.ships[shipName]; i++) {
+        locationArray.push(location + i * 10);
+      }
+    }
+    return locationArray;
+  }
+
+  isValidPlacement(locationArray) {
+    if (locationArray.length <= 0) {
+      return false;
     }
 
-    build(){
-        for (let i=0; i<100; i++){
-            this.board.push({hasShip: false, isHit: false})
-        }
+    const wall = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99];
+
+    if (locationArray.some((loc) => !this.board[loc])) {
+      return false;
     }
 
-    receiveAttack(location){
-        this.board[location].isHit = true
-        if(this.board[location].hasShip !== true){
-            return
-        }
-        else{
-            this.attackShip(location)
-        } 
+    if (locationArray.some((loc) => this.board[loc].hasShip)) {
+      return false;
     }
 
-    attackShip(location){
-        this.fleet.forEach((boat,i)=>{
-            if (!boat.position.includes(location)){
-                return
-            }
-            else{
-                boat.hit(location)
-                if (!boat.isSunk()){
-                    return
-                }
-                else {
-                    this.fleet.slice(i,1)
-                    console.log(`${boat.name} sank`)
-                }
-            }
-        })
-
+    if (
+      wall.some((num) => {
+        return [num, num + 1].every((combo) => locationArray.includes(combo));
+      })
+    ) {
+      return false;
     }
 
-    placeShip(location, shipName, axis){
-        let ship = new Ship(shipName, this.ships[shipName])
-        let locationArray = this.createLocationArray(location, ship, axis)
-            if (!this.checkPlacement(locationArray)){
-                return
-            }
-            else{
-                locationArray.forEach(loc =>{
-                    this.board[loc].hasShip = true
-                    ship.position.push(loc)
-                })
-            }
-        this.fleet.push(ship)
-    }
+    return true;
+  }
 
-    createLocationArray(location, ship, axis){
-        const locationArray = []
-        if (axis === 'x'){
-            for(let i=0; i <ship.size; i++){
-                locationArray.push(location + i)
-            }
-        }
-        else{
-            for(let i=0; i < ship.size;i++){
-                locationArray.push(location + i * 10)
-            }
-        }
-        return locationArray
-    }
+  computerShipPlacement() {
+    let axis = ["x", "y"];
 
-    checkPlacement (locationArray){
+    let shipNames = Object.keys(this.ships);
 
-        const wall = [9,19,29,39,49,59,69,79,89,99]
+    shipNames.forEach((shipName) => {
+      let locationArray = [];
+      let randomLoc = 0;
+      let randomAxis = 0;
 
-        if(locationArray.some((loc) => !this.board[loc])){
-            return false
-        }
-        else if (locationArray.some((loc)=>this.board[loc].hasShip)){
-            return false
-        }
-        else if (wall.some((num)=>{
-            return [num, num+1].every((combo)=>
-                locationArray.includes(combo))
-        })
-        ){
-            return false
-        }
-        else{
-            return true
-        }
-    }
-
-    computerShipPlacement(){
-        let axis = ['x','y']
-
-        let ships = Object.keys(this.ships)
-        let i =0
-
-        ships.forEach(ship =>{
-            while (true){
-                let randomLoc = Math.floor(Math.random()*99)
-                let randomAxis = Math.floor(Math.random()*2)
-                let locationArray = this.createLocationArray(randomLoc, ship, axis[randomAxis])
-
-                if (!this.checkPlacement(locationArray)){
-                    return true
-                }
-                else{
-                    this.placeShip(randomLoc, ship, axis[randomAxis])
-                }
-            }
-        })
-    }
+      while (!this.isValidPlacement(locationArray)) {
+        randomLoc = Math.floor(Math.random() * 99);
+        randomAxis = Math.floor(Math.random() * 2);
+        locationArray = this.createLocationArray(
+          randomLoc,
+          shipName,
+          axis[randomAxis]
+        );
+      }
+      this.placeShip(randomLoc, shipName, axis[randomAxis]);
+    });
+  }
 }
 
-export {Gameboard}
+export { Gameboard };
